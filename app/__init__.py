6bb82a0facb9c52b37e91a68c1761467651a0f97
vicky <victoria.awuor@student.moringaschool.com>
2022-03-05 13:15:18 +0300

    created class user and passed the db.model to allow communication with the database

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask import Flask
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+
+    #initialising the flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
