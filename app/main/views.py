9f0bbae4321fcf8b51de9928670285b7d19de133
vicky <victoria.awuor@student.moringaschool.com>
2022-03-06 18:02:07 +0300

    installed flsk uploads extension

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,46 @@
-from flask_login import login_required
+from flask_login import current_user, login_required
 import main
+from flask import render_template,request,redirect,url_for,abort
+from ..models import User, Pitch
+from .forms import UpdateProfile
+from .. import db
 
-@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+
+@main.route('/user/<username>')
 @login_required
-def new_review(id):
+def profile(username):
+    user = User.query.filter_by(username = username).first()
+    word = Pitch.query.filter_by(user = current_user).all()
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user,word=word)
+
+@main.route('/')
+def index():
+    pitches = Pitch.query.all()
+    interview = Pitch.query.filter_by(category = 'Interview').all()
+    product = Pitch.query.filter_by(category = 'Product').all() 
+    project = Pitch.query.filter_by(category = 'Project').all()
+    promotion = Pitch.query.filter_by(category = 'Promotion').all() 
+    
+    return render_template('index.html',pitches = pitches, interview = interview,product = product,project = project, promotion = promotion)
+
+@main.route('/user/<username>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(username):
+    user = User.query.filter_by(username = username).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',username=user.username))
+
+    return render_template('profile/update.html',form =form)
--- app/models.py
+++ app/models.py
@@ -18,6 +18,8 @@ class User(UserMixin,db.Model):                      #connecting our class to th
     pass_secure = db.Column(db.String(255))
     password_hash = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     downvote = db.relationship('Downvote',backref = 'user',lazy="dynamic")
     upvote= db.relationship('Upvote',backref = 'user',lazy="dynamic")
     comment= db.relationship('Comment',backref = 'user',lazy="dynamic")
