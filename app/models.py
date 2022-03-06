bc33f0d15c379dac5a74f94a18631bade5d61f60
vicky <victoria.awuor@student.moringaschool.com>
2022-03-06 14:24:01 +0300

    created hashing functionality for the password

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 
@@ -7,10 +8,23 @@ class User(db.Model):                      #connecting our class to the database
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
     downvote = db.relationship('Downvote',backref = 'user',lazy="dynamic")
     upvote= db.relationship('Upvote',backref = 'user',lazy="dynamic")
     comment= db.relationship('Comment',backref = 'user',lazy="dynamic")
     pitch= db.relationship('Pitch',backref = 'user',lazy="dynamic")
+
+    @property
+    def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
 
 
