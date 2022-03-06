b13c608f027030e832a03b4998a30a253e200da0
vicky <victoria.awuor@student.moringaschool.com>
2022-03-06 10:16:48 +0300

    installed flask-migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask import Flask
+from app import app
+
 
 bootstrap = Bootstrap()
-db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///pitches.db'
+db = SQLAlchemy(app)
 
 def create_app(config_name):
     app = Flask(__name__)
-
+    
 
     #initialising the flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+
+from app import views
+from app import models
--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from app import create_app, db
-from app.models import User
+from app.models import User , Pitch , Upvote, Downvote, Comment
 from flask_script import Manager
 
 
@@ -7,6 +7,6 @@ manager = Manager(app)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User, Pitch = Pitch, Upvote = Upvote, Downvote = Downvote, Comment = Comment )
 if __name__ == '__main__':
     manager.run()
