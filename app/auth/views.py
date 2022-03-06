3eb9353524357311d60a12cf99a32e271fda56ca
vicky <victoria.awuor@student.moringaschool.com>
2022-03-06 17:16:23 +0300

    added the authrntication route for user logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ from . import auth
 from .forms import RegistrationForm, LoginForm
 from .. import db
 from ..models import User
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 
 @auth.route('/login')
 def login():
@@ -35,3 +35,9 @@ def login():
 
     title = "Pitch Login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
