47deb93c05d07c2d5243b718f7d8954ac068570b
vicky <victoria.awuor@student.moringaschool.com>
2022-03-06 15:29:15 +0300

    created registration form for the user class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,4 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import Required,Email,EqualTo
+from wtforms.validators import Required,Email,EqualTo, ValidationError
 from ..models import User
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
