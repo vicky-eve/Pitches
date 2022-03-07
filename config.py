9eaaa704a53c0e70cafa9144e1fa839e6e445ec1
vicky <victoria.awuor@student.moringaschool.com>
2022-03-07 09:36:34 +0300

    installed simplemde markdown editor

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
+import os 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vicky:password@localhost/pitches'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
