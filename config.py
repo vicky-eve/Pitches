d1535159c3a8c2bca361611ed0ebcfe7870cb8a8
vicky <victoria.awuor@student.moringaschool.com>
2022-03-11 14:48:31 +0300

    added production configuration

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     #  email configurations
@@ -12,14 +13,24 @@ class Config:
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
 
+class TestConfig(Config):
+    pass
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
 
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vicky:aderazi@localhost/pitches'
     DEBUG = True
 
-config_options = {
-'development':DevConfig
 
-# 'test':TestConfig
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
 }
