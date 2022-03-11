061b92a708d27b92da0abdcfce4f4e899ebc78b4
vicky <victoria.awuor@student.moringaschool.com>
2022-03-11 17:03:38 +0300

    all changes

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config:
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
 
 
